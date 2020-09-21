dbb960870f264d01a3c7a83376df5a9e50dbb671
joannaylin <60360277+joannaylin@users.noreply.github.com>
2020-09-20 23:44:04 -0500

    Update and rename leetcode-811-m.js to leetcode-811-e.js

--- exercises/leetcode-811-e.js
+++ exercises/leetcode-811-e.js
@@ -1,4 +1,4 @@
-// LeetCode 811: Subdomain Visit Count (Medium)
+// LeetCode 811: Subdomain Visit Count (Easy)
 
 /*
 A website domain like "discuss.leetcode.com" consists of various subdomains. At the top level, we have "com", at the next level, we have "leetcode.com", and at the lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com", we will also visit the parent domains "leetcode.com" and "com" implicitly.
